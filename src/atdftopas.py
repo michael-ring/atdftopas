708beef10816555ca1bc487981f44f56b01ce164
Michael Ring <mail@michael-ring.org>
2020-12-27 20:22:16 +0100

    Added Microchip license

--- src/atdftopas.py
+++ src/atdftopas.py
@@ -92,6 +92,22 @@ def main(argv):
     unifyInterrupts(interrupts)
 
   print(f"unit {chip.lower().replace('atsam','sam')};")
+  print("""(*
+Copyright (c) 2020 Microchip Technology Inc.
+                   
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the Licence at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*)
+""")
   print("interface")
   print("{$PACKRECORDS C}")
   print("{$GOTO ON}")
